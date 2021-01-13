e1dc58715148fc9889e504689e7b26c872f43710
Philipp Rollinger <philipp.rollinger@gmail.com>
2021-01-13 13:06:30 +0100

    Make Frontpage describing braingain

--- memo/users/models.py
+++ memo/users/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    """Default user for Memo."""
+    """Default user for BrainGain."""
 
     #: First and last name do not cover name patterns around the globe
     name = CharField(_("Name of User"), blank=True, max_length=255)
